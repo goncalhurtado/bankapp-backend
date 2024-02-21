f4f022031c6108c478a9afa55ee5d58ed28eaa22
goncal hurtado <goncalhurtado@gmail.com>
2024-02-21 19:10:10 -0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+// const { getAllUsers, registerUser } = require("../controllers/userController");
+
+// router.get("/users", getAllUsers);
+// router.post("/user", registerUser);
+
+module.exports = router;
